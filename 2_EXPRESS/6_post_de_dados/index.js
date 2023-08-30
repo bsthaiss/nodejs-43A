364271f9e8b63aa317c9b75eb2dd5653de0bf495
Thais Barbosa dos Santos <thaisbs1405@gmail.com>
2023-08-29 22:00:12 -0300

    Criei uma nova pasta

--- 2_EXPRESS/6_post_de_dados/index.js
+++ 2_EXPRESS/6_post_de_dados/index.js
@@ -2,41 +2,18 @@ const express = require('express')
 const app = express()
 
 const path = require('path')
-const caminho = path.join(__dirname, 'templates')
+const basePath = path.join(__dirname, 'templates')
 
-// acessar as informações do corpo da requisição
-app.use(express.urlencoded({
-    extended: true
-}))
-
-// transforma as informações em objetos do JS
-app.use(express.json())
-
-app.post('/users/save', (req, res) => {
-    const nome = req.body.nome
-    const idade = req.body.idade
-
-    console.log(`
-        Usuário: ${nome}
-        Idade: ${idade}
-    `)
-
-    res.redirect('/')
-})
-
-app.get('/users/cadastrar', (req, res) => {
-    res.sendFile(`${caminho}/usuariosform.html`)
-})
-
-app.get('/users/:id', (req, res) => {
+app.get('/usuarios/:id', (req, res) => {
     const id = req.params.id
 
-    console.log(`Estamos buscando pelo usuário: ${id}`)
-    res.sendFile(`${caminho}/usuarios.html`)
+    console.log(`Usuário: ${id}`)
+
+    res.sendFile(`${basePath}/usuarios.html`)
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(`${caminho}/index.html`)
+    res.sendFile(`${basePath}/index.html`)
 })
 
 app.listen(3000)
