e88f615ec29f508a963be5171a468d08faa86437
Thais Barbosa dos Santos <thaisbs1405@gmail.com>
2023-08-29 22:36:30 -0300

    Criei novo arquivo

--- 2_EXPRESS/6_post_de_dados/index.js
+++ 2_EXPRESS/6_post_de_dados/index.js
@@ -4,6 +4,26 @@ const app = express()
 const path = require('path')
 const basePath = path.join(__dirname, 'templates')
 
+// Recebe informaçãoes do corpo da requisição. São funções intermediárias.
+app.use(express.urlencoded({
+    extended: true
+}))
+
+// Converte em objeto do JS
+app.use(express.json())
+
+app.post('/usuarios/enviar', (req, res) => {
+    const nome = req.body.nome
+    const email = req.body.email
+
+    console.log(`
+        Usuário: ${nome}
+        Email: ${email}
+    `)
+
+    res.redirect('/')
+})
+
 app.get('/usuarios/cadastrar', (req, res) => {
     res.sendFile(`${basePath}/form.html`)
 })
