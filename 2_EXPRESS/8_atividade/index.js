f99a65f1233dc8575fbf99732327771177147564
Thais Barbosa dos Santos <thaisbs1405@gmail.com>
2023-09-26 21:39:50 -0300

    Aula 26/09

--- 2_EXPRESS/8_atividade/index.js
+++ 2_EXPRESS/8_atividade/index.js
@@ -2,22 +2,19 @@ const express = require('express')
 const app = express()
 
 const path = require('path')
-const caminho = path.join(__dirname, 'templates')
+const basePath = path.join(__dirname, 'templates')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(express.static(publicPath))
 
 app.use(express.static('public'))
 
 app.get('/cadastrar', (req, res) => {
-    res.sendFile(path.join(templatesPath, 'index.html'))
+    res.sendFile(`${basePath}/index.html`)
 })
 
 app.post('/cadastrar', (req, res) => {
-    const nome = req.body.nome;
-    const email = req.body.email;
-    const senha = req.body.senha;
+    const {nome, email, senha} = req.body
 
     console.log(`
         Usuário: ${nome}
@@ -29,7 +26,7 @@ app.post('/cadastrar', (req, res) => {
 })
 
 app.get('/premio', (req, res) => {
-    res.sendFile(path.join(templatesPath, 'premio.html'));
+    res.sendFile(`${basePath}/premio.html`);
 })
 
 app.listen(5000)
